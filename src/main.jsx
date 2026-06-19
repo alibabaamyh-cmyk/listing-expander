3c6da6bf9078df44976614cc623d408b75b2d059
Amy <amy72530@gmail.com>
2026-06-19 13:56:55 +0800

    debug: add error boundary to show runtime errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) { super(props); this.state = { error: null }; }
+  static getDerivedStateFromError(e) { return { error: e }; }
+  render() {
+    if (this.state.error) {
+      return React.createElement('pre', {
+        style: { padding: 32, color: 'red', background: '#fff', whiteSpace: 'pre-wrap' }
+      }, '錯誤：' + this.state.error.message + '\n\n' + this.state.error.stack);
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 )
